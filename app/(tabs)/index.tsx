e751e4b3bece992b5d0b808a704148b44e1f1688
gfrnca <gabrielbfranca27@gmail.com>
2024-06-03 19:48:17 -0300

    refactor: removing useless code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,7 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, View } from 'react-native'
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+  return <View style={styles.container}></View>
 }
 
 const styles = StyleSheet.create({
@@ -28,4 +19,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+})
