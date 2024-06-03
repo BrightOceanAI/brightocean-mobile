db546fb7dbc61fd53b0f4d024a769245c6358143
gfrnca <gabrielbfranca27@gmail.com>
2024-06-03 20:55:37 -0300

    feat: background image to auth choice screen

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -5,13 +5,16 @@ import { ImageBackground, StyleSheet } from 'react-native'
 export default function AuthChoice() {
   return (
     <View style={{ flex: 1 }}>
-      <ImageBackground style={styles.backgroundImage}></ImageBackground>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={require('@/assets/images/auth/auth-bg.jpg')}
+      ></ImageBackground>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {},
+  backgroundImage: { flex: 1 },
 
   container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
 })
